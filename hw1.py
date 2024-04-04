98bc6d59019eda39be090f71339e90e7d6bb492c
Lyla Liu <lylaliu@Lylas-MBP.lan>
2024-04-03 21:19:24 -0700

    problem 1

---
+++ hw1.py
@@ -0,0 +1,28 @@
+
+# Problem 1
+# Set the base conditions for N = 0, 1, 2 
+def PAD(N):
+    if N == 0 or N == 1 or N == 2:
+        return 1
+    else:
+        return PAD(N-2) + PAD(N-3)
+
+# Probelm 2
+def SUMS(N):
+    if N == 0 or N == 1 or N == 2:
+        return 0
+    else:
+        return 0
+
+# Problem 3
+
+
+
+
+# Problem 4
+
+
+
+# Problem 5
+
+
